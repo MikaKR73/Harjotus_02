54b2aa9c347f0f02ba9cdd0e89a7134230b59304
Mika Rasinmäki <mika.rasinmaki@ictpro.labra>
2020-11-18 12:23:32 +0200

    Toimiva versio

--- Harjoitus_02/Harjoitus_02.ino
+++ Harjoitus_02/Harjoitus_02.ino
@@ -4,12 +4,15 @@
 
 void setup(){
 pinMode(LED1, OUTPUT);
+Serial.begin(9600);
 }
 int viive = 800;
 
 void loop(){
   digitalWrite(LED1, LEDON);   // turn the LED on (HIGH is the voltage level)
+  Serial.println("LEDI ON");
   delay(viive);
   digitalWrite(LED1, LEDOFF);    // turn the LED off by making the voltage LOW
+  Serial.println("LEDI OFF");
   delay(viive);  
 }
