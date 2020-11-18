648986a0050108cec8c69daafb5d4a4d120de314
Mika Rasinmäki <mika.rasinmaki@ictpro.labra>
2020-11-18 14:27:25 +0200

    Pidemälle kehitetty versio

--- Harjoitus_02/Harjoitus_02.ino
+++ Harjoitus_02/Harjoitus_02.ino
@@ -1,18 +1,55 @@
-#define LED1 6 // Portti
-#define LEDON HIGH //Päälle
-#define LEDOFF LOW //Pois
-
-void setup(){
-pinMode(LED1, OUTPUT);
-Serial.begin(9600);
+/*
+ * Ohjelmassa luetaan sarjaporttiin tulleita arvoja. Luettu arvo 
+ * tallentuu data nimiseen muuttujaan. Ohjelmassa tehdään testi 
+ * jolla varmistetaan että tulostus tapahtuu ainoastaan silloin
+ * kun luettu arvo ei ole yhtäkuin -1.
+ */
+ #define led1 6
+ #define ledon LOW
+ #define ledoff HIGH
+ #define VIIVE 500
+void setup() {
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);
 }
-int viive = 800;
 
-void loop(){
-  digitalWrite(LED1, LEDOFF);   // turn the LED on (HIGH is the voltage level)
-  Serial.println("LEDI ON");
-  delay(viive);
-  digitalWrite(LED1, LEDON);    // turn the LED off by making the voltage LOW
-  Serial.println("LEDI OFF");
-  delay(viive);  
+void loop() {
+  int data = Serial.read();
+
+// if-testi ehdollistaa käskyjen suorituksen
+ if(data == 'R' || data == 'r' || data == 'S' || data == 's'){
+   Serial.println("Vilkuttaa");
+    digitalWrite(led1,ledon);
+    delay(VIIVE);
+    digitalWrite(led1, ledoff);
+     delay(VIIVE); 
+    digitalWrite(led1,ledon);
+    delay(VIIVE);
+    digitalWrite(led1,ledon);
+     delay(VIIVE);
+    digitalWrite(led1, ledoff); 
+    delay(VIIVE);
+    digitalWrite(led1,ledon);
+     delay(VIIVE);
+    digitalWrite(led1, ledoff); 
+    delay(VIIVE);
+    digitalWrite(led1,ledon);
+     delay(VIIVE);
+    digitalWrite(led1, ledoff); 
+    data = -1;
+ }
+
+if(data == 10){ // Lukee enterin painaluksen
+   data = -1 ;
+    Serial.println("Enter");
+    digitalWrite(led1,ledon);
+    delay(1500);
+    digitalWrite(led1, ledoff);
+  }
+ 
+
+    if(data != -1){ //Poistaa -1 merkin ja tulostaa muut merkit
+    
+    Serial.println(data);
+    }
 }
